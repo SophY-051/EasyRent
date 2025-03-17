e6838b4f2792ce6c0031d057b154d149f3c23c4f
SophY-051 <82739572+SophY-051@users.noreply.github.com>
2025-03-18 00:03:47 +0100

    17.03.2025 Auto Objekte in AutoReg initialisiert und XAML angepasst

--- proj/Auto.cs
+++ proj/Auto.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace EasyRentProj
 {
-    class Auto
+    public class Auto
     {
         //SR 17.03.25 Eigenschafen der Auto-Klasse
-        public int id { get; set; }
-        public string marke { get; set; }
-        public string model { get; set; }
-        public string getriebe { get; set; }
-        public int sitze { get; set; }
-        public decimal preis { get; set; }
+        public int autoID {get; set;}
+        public string autoMarke {get; set;}
+        public string autoModel {get; set;}
+        public string autoGetriebe { get; set;}
+        public int autoSitze {get; set;}
+        public decimal autoPreis {get; set;}
       
 
 
--- proj/Auto_Registrierung.xaml.cs
+++ proj/Auto_Registrierung.xaml.cs
@@ -22,26 +22,52 @@ namespace EasyRentProj
     /// </summary>
     /// 
 
-     //SR 18.02.2025 Screen erstellt und mit API MahApp verbunden
-    public partial class Auto_Registrierung : MetroWindow       
- 
+     
+    public partial class Auto_Registrierung : MetroWindow
+    {
+        //SR 18.02.2025 Screen erstellt und mit API MahApp verbunden
+        //SR 18.02.2025 Screen erstellt und mit API MahApp verbunden
+        /*private void TextBox_TextChanged()
+        {
+
+        }*/
         public Auto_Registrierung()
         {
-           InitializeComponent();
+            InitializeComponent();
 
-            //Variabeln die alle wichtigen Daten für die Autoregistrierung speichert
+            //SR 17.03.2025 new Auto-Objekt initalisieren
+            Auto auto1 = new Auto();
+            auto1.autoID = 001;
+            auto1.autoMarke = "VW";
+            auto1.autoModel = "Golf VI";
+            auto1.autoGetriebe = "S";
+            auto1.autoSitze = 4;
+            auto1.autoPreis = 40;
 
+            CarRegGridXAML.Items.Add(auto1);
 
-            public string registratedCars
+            Auto auto2 = new Auto();
+            auto1.autoID = 002;
+            auto1.autoMarke = "Audi";
+            auto1.autoModel = "A1 Sport Paket, 2024";
+            auto1.autoGetriebe = "A";
+            auto1.autoSitze = 4;
+            auto1.autoPreis = 124;
+
+            CarRegGridXAML.Items.Add(auto2);
+        }
+
+        /*SR 18.02.2025 Variabeln die alle wichtigen Daten für die Autoregistrierung speichert
+        public string registratedCar
             {
                 get
                 {
                 return $"{Marke} {Model}";
                 }
                 
-            }
+            } */
+
 
 
     }
-
 }

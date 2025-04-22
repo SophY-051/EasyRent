4777b6d68aa73ba35dbad64d41885e2f70bb3e58
robertbnv <adamrobert0@icloud.com>
2025-04-22 21:19:41 +0200

    RA 22.04.2025 Rückgabe

---
+++ proj/RueckgabenSQLData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace EasyRentProj
+{
+    public class RueckgabeSQLData : DbContext
+    {
+        //RA 22.04.2025 Pfad zur SQLite-Datenbank über Umgebungsvariable
+        public static string path = ConfigurationManager.AppSettings["RENT_DB_PATH"];
+        
+        //RA 22.04.2025 Konfiguration der Datenbankverbindung
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={path}");
+        }
+        
+        //RA 22.04.2025 DbSet für die Rueckgabe-Tabelle
+        public DbSet<Rückgabe> Rueckgaben { get; set; }
+    }
+}

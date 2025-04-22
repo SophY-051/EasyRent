67cd7136932b482317a6c0b95590672b065a0e70
robertbnv <adamrobert0@icloud.com>
2025-04-22 19:22:13 +0200

    RA 22.04.2025 Login Screen gefixed

--- proj/UserLoginSQLData.cs
+++ proj/UserLoginSQLData.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,13 @@ using System.Threading.Tasks;
 namespace EasyRentProj
 {
     public class UserLoginSQLData : DbContext
+
     {
+        public static string path = ConfigurationManager.AppSettings["RENT_DB_PATH"];
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source=Data.db");
+            optionsBuilder.UseSqlite($"Data Source={path}");
         }
 
         public DbSet<User> User { get; set; }

9e240e4d79704f05b11e70e219ab938bd6ac1a1c
Adam <adam.robert@softwareschmiede.com>
2025-04-23 16:15:41 +0200

    a

--- proj/App.xaml.cs
+++ proj/App.xaml.cs
@@ -10,11 +10,7 @@ namespace EasyRentProj
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            DatabaseFacade facade = new DatabaseFacade(new UserLoginSQLData());
-            facade.EnsureCreated();
-        }
+        
     }
 
 }
